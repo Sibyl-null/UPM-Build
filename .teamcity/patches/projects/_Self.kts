52dc8915dda6d51d26a1b3cf8dbbf1bdc85d5617
Sibyl <Sibyl@TeamCity>
2025-04-26 21:10:06 +0800

    project parameters were changed (TeamCity change in 'UPM Build' project)

--- .teamcity/patches/projects/_Self.kts
+++ .teamcity/patches/projects/_Self.kts
@@ -21,7 +21,7 @@ changeProject(DslContext.projectId) {
             param("UnityPath", """C:\Program Files\Unity\Hub\Editor\2022.3.24f1c1\Editor\Unity.exe""")
         }
         update {
-            text("UnityPath", "UnityPath", readOnly = true, allowEmpty = true)
+            param("UnityPath", "UnityPath")
         }
     }
 }
