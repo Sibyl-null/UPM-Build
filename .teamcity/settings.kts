afac91a901150f78e9987c4b35bbe780d8565344
Sibyl-null <1286438810@qq.com>
2025-05-04 19:36:02 +0800

    [sandbox]: TeamCity Config changed

--- .teamcity/settings.kts
+++ .teamcity/settings.kts
@@ -2,28 +2,6 @@ import jetbrains.buildServer.configs.kotlin.*
 import jetbrains.buildServer.configs.kotlin.buildFeatures.perfmon
 import jetbrains.buildServer.configs.kotlin.buildSteps.script
 
-/*
-The settings script is an entry point for defining a TeamCity
-project hierarchy. The script should contain a single call to the
-project() function with a Project instance or an init function as
-an argument.
-
-VcsRoots, BuildTypes, Templates, and subprojects can be
-registered inside the project using the vcsRoot(), buildType(),
-template(), and subProject() methods respectively.
-
-To debug settings scripts in command-line, run the
-
-    mvnDebug org.jetbrains.teamcity:teamcity-configs-maven-plugin:generate
-
-command and attach your debugger to the port 8000.
-
-To debug in IntelliJ Idea, open the 'Maven Projects' tool window (View
--> Tool Windows -> Maven Projects), find the generate task node
-(Plugins -> teamcity-configs -> teamcity-configs:generate), the
-'Debug' option is available in the context menu for the task.
-*/
-
 version = "2025.03"
 
 project {
@@ -36,8 +14,16 @@ object UpmBuild_AndroidDebug : BuildType({
     name = "Android - 底包 - Debug"
 
     params {
-        select("Mode", "Mode",
-                options = listOf("Debug", "Release"))
+        select(
+            name = "Mode",
+            value = "Debug",
+            options = listOf("Debug", "Release"))
+
+        checkbox(
+            name = "IsAppBundle",
+            value = "false",
+            checked = "true",
+            unchecked = "false")
     }
 
     vcs {
@@ -59,7 +45,7 @@ object UpmBuild_AndroidDebug : BuildType({
                     -- ^
                     --appStore=Google ^
                     --mode=%Mode% ^
-                    --isAppBundle=false
+                    --isAppBundle=%IsAppBundle%
             """.trimIndent()
         }
     }
